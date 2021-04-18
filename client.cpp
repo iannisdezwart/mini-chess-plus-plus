f79b9778eef28aeb49d5cda0d5216a294566a7eb
iannisdezwart <iannis__@hotmail.com>
2021-04-18 13:51:38 +0200

    🚀 Implemented Creating/Joining Game & Playing Game

--- client.cpp
+++ client.cpp
@@ -4,18 +4,46 @@
 
 #include "keypress.hpp"
 #include "board.hpp"
+#include "game.hpp"
 #include "event-emitter.hpp"
 #include "websocket-client.hpp"
+#include "util.hpp"
+#include "ws_messages.hpp"
 
-events::EventEmitter<uint8_t> key_pressed;
-
-void keypress_thread()
+void game(WebsocketClient& ws_client, std::string&& room_name,
+	enum chess::Players player)
 {
-	while (true)
+	chess::Game *game = new chess::Game(player, ws_client, std::move(room_name));
+
+	ws_client.message_event.set_listener([&ws_client, game](std::string& message)
 	{
-		uint8_t key = keypress::get_key();
-		key_pressed.trigger(key);
-	}
+		if (util::starts_with(message, "move "))
+		{
+			if (message.size() < 10)
+			{
+				debug("invalid length in server move message");
+				goto read_next;
+			}
+
+			try
+			{
+				ws_messages::move::Move move = ws_messages::move::decode_message(message);
+
+				game->move(move.from, move.to);
+				goto read_next;
+			}
+			catch (...)
+			{
+				debug("invalid server move message");
+				goto read_next;
+			}
+		}
+
+		read_next:
+		ws_client.do_read();
+	});
+
+	ws_client.do_read();
 }
 
 void create_room(WebsocketClient& ws_client)
@@ -24,22 +52,44 @@ void create_room(WebsocketClient& ws_client)
 	std::string room_name;
 	std::cin >> room_name;
 
-	ws_client.write("create-room " + room_name);
-
-	ws_client.message_event.add_listener([&ws_client](std::string& message) {
-		if (message == "ok")
+	ws_client.message_event.set_listener(
+		[&ws_client, room_name](std::string& message) mutable
+	{
+		if (message == ws_messages::general::ok_message)
 		{
-			printf("successfully joined room\n");
+			game(ws_client, std::move(room_name), chess::Players::WHITE);
 		}
 		else
 		{
-			printf("error joining room\n");
+			printf("error creating room: %s\n", message.c_str());
 		}
-
-		ws_client.close();
 	});
 
-	// ws_client.do_read();
+	ws_client.write("create-room " + room_name);
+	ws_client.do_read();
+}
+
+void join_room(WebsocketClient& ws_client)
+{
+	printf("Room name > ");
+	std::string room_name;
+	std::cin >> room_name;
+
+	ws_client.message_event.set_listener(
+		[&ws_client, room_name](std::string& message) mutable
+	{
+		if (message == ws_messages::general::ok_message)
+		{
+			game(ws_client, std::move(room_name), chess::Players::BLACK);
+		}
+		else
+		{
+			printf("error joining room: %s\n", message.c_str());
+		}
+	});
+
+	ws_client.write("join-room " + room_name);
+	ws_client.do_read();
 }
 
 int main()
@@ -61,7 +111,7 @@ int main()
 				break;
 
 			case '2':
-				// join_room();
+				join_room(ws_client);
 				break;
 
 			default:
