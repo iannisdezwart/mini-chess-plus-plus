edeeede967f77a7cfc09fc917bd42c8d249f8458
iannisdezwart <iannis__@hotmail.com>
2021-04-18 13:50:39 +0200

    🧹 Moved Util Functions Into Their Own File

---
+++ util.hpp
@@ -0,0 +1,21 @@
+#ifndef UTIL_HEADER
+#define UTIL_HEADER
+
+#include <bits/stdc++.h>
+
+namespace util
+{
+	bool starts_with(std::string str, std::string search)
+	{
+		if (search.size() > str.size()) return false;
+
+		for (size_t i = 0; i < search.size(); i++)
+		{
+			if (str[i] != search[i]) return false;
+		}
+
+		return true;
+	}
+};
+
+#endif
